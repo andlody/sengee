dd17f110c0e5647142328712c723180987147a50
Andree Ochoa <andree.ochoa@externos.bci.cl>
2022-12-15 11:15:24 -0500

    add git

---
+++ _libs/kurmix/ReqRes.php
@@ -0,0 +1,121 @@
+<?php
+/*===== Kurmix - PHP =====                           _  __   www.kurmix.com   _      
+* @author    Andree Ochoa <andlody@hotmail.com>     | |/ /   _ _ __ _ __ ___ (_)_  __
+* @copyright 2017-2018 Andree Ochoa                 | ' / | | | '__| '_ ` _ \| \ \/ /
+* @license   The MIT license                        | . \ |_| | |  | | | | | | |>  < 
+* @version   1.0.0                                  |_|\_\__,_|_|  |_| |_| |_|_/_/\_\       */
+
+class ReqRes{
+
+	protected $view;
+	protected $template;
+	protected $data;
+
+	function __construct(){
+		$this->template='default';
+		$this->data=new Data();
+	}
+
+	public function show(){
+		if($this->view == null) return;
+
+		$url="view/".$this->view;
+
+		if($this->template!=null){
+            $url = "view/_templates/".$this->template;
+        }
+		
+		require_once("_libs/kurmix/Views.php");
+		$v = new Views($this->data);
+		
+		//============= Vista php =============
+		if($this->template!=null){$v->setKurmix($this->view); }
+
+		if(!file_exists('app/'.$url.'.php')){
+			$obj = new Controller();
+			$obj->setKurmix("",array(401,$this->template,$this->view)); return;
+		}
+
+        include('app/'.$url.'.php');        
+	}
+
+	public function write($value){
+ 		echo $value;
+ 	}
+
+ 	public function setView($view){
+ 		$this->view = $view;
+ 	}
+
+ 	public function setTemplate($temp){
+ 		$this->template = $temp;
+ 	}
+
+	public function redirect($url){
+		if(trim($url)!='') 
+			$url='k='.$url;
+ 		header("Location: ?".$url);
+ 	}
+
+ 	function getParameter($name){
+ 		if(!empty($_GET[$name])) return $_GET[$name];
+
+ 		if(!empty($_POST[$name])) return $_POST[$name];
+ 					
+		return null;
+	}
+
+	public function setData($data1,$data2=null){
+ 		$this->data->set($data1,$data2);
+ 	}
+
+ 	public function getData($index){
+ 		return $this->data->get($index);
+ 	}
+
+ 	public function json(){
+ 		return $this->data->json();
+ 	}
+
+ 	public static function error($a){
+ 		if(!Config::DEV) { 			
+			self::error404();
+ 		}
+		$obj = new Controller();
+ 		switch ($a[0]) {
+ 			case 101:
+ 				$obj->write("ERROR: En el Router, no existe la accion:<strong> ".$a[1]." </strong> en el controlador:<strong> ".$a[2]."</strong><br>");
+ 				break;
+ 			case 103:
+ 				$obj->write("ERROR: En el Router, no existe el controlador:<strong> ".$a[1]."</strong><br>");
+ 				break;
+ 			case 201:
+                $obj->write("ERROR: En el controlador al instanciar el modelo:<strong> ".$a[1]."</strong> - Posiblemente no existe.<br>");
+                break;
+            case 202:
+                $obj->write("ERROR: En el controlador al instanciar la libreria:<strong> ".$a[1]."</strong> - Posiblemente no existe.<br>");
+                break;
+            case 301:
+                $obj->write("ERROR: En el modelo, al realizar la consulta:<strong> ".$a[1]." </strong>.<br>".$a[2]."<br>");
+                break;
+            case 306:
+                $obj->write("ERROR: En el modelo al llamar:<strong> ".$a[1]."</strong><br>".$a[2]."<br>");
+                break;
+            case 401:
+            	$obj->write("ERROR: En la vista, Template:<strong> ".$a[1]." </strong> | Vista: <strong>".$a[2]."</strong><br>");
+                break;
+            case 402:
+                $obj->write("ERROR: En la vista, al llamar a: <strong> ".$a[1]." </strong> - Posiblemente no existe la vista.<br>");
+                break;            
+ 		}
+ 		die();
+ 	}
+
+ 	public static function error404(){
+ 		$r = new ReqRes();
+ 		$r->setTemplate(null);
+ 		$r->setView("_templates/_404");
+ 		$r->show(); 
+ 		die();
+ 	}
+}
---
+++ _libs/kurmix/Views.php
@@ -0,0 +1,52 @@
+<?php
+/*===== Kurmix - PHP =====                           _  __   www.kurmix.com   _      
+* @author    Andree Ochoa <andlody@hotmail.com>     | |/ /   _ _ __ _ __ ___ (_)_  __
+* @copyright 2017-2018 Andree Ochoa                 | ' / | | | '__| '_ ` _ \| \ \/ /
+* @license   The MIT license                        | . \ |_| | |  | | | | | | |>  < 
+* @version   1.0.0                                  |_|\_\__,_|_|  |_| |_| |_|_/_/\_\       */
+
+class Views {
+    protected   $data;
+    public      $controller;
+    public      $action;
+    public      $body;
+    
+    function __construct($data){
+        $this->data = $data;
+    }
+
+    function get($val=null){
+        return $this->data->get($val);
+    }
+
+    function set($name,$value){
+        return $this->data->set($name,$value);
+    }
+
+    function size($val=null){
+        return $this->data->size($val);
+    }
+
+    function partial($val){
+        return "app/view/_partials/".$val.".php";
+    }
+
+    function setKurmix($val){
+            $this->body="app/view/".$val.".php";
+            if(!file_exists($this->body)){
+                $obj = new Controller();
+                $obj->setKurmix("",array(402,$this->body)); return;
+            }
+
+            $a = explode("/", $val);
+            
+            if(sizeof($a)<2){
+                $obj = new Controller();
+                $obj->setKurmix("",array(401,"",$val)); return;
+            }
+
+            $this->controller = $a[0];
+            $this->action = $a[1];
+    }   
+
+}
